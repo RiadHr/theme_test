5071fb439440256f05b115ea50832c48ef8b6bda
John Smith <a.harmel@esi-sba.dz>
2024-05-31 15:50:45 +0100

    third commit commit

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -17,20 +17,19 @@ class ThemeProvider extends ChangeNotifier{
 class MyTheme{
   static final lightTheme = ThemeData(
     scaffoldBackgroundColor: Colors.white,
-    colorScheme: ColorScheme.light(),
-    iconTheme: IconThemeData(color: Colors.orange,size: 30 ),
-    appBarTheme: AppBarTheme(
+    colorScheme: const ColorScheme.light(),
+    iconTheme: const IconThemeData(color: Colors.orange,size: 30 ),
+    appBarTheme: const AppBarTheme(
       backgroundColor: Colors.white60,
         iconTheme: IconThemeData(color: Colors.orange,size:40 ))
-
   );
 
   static final darkTheme = ThemeData(
     scaffoldBackgroundColor: Colors.black12,
     colorScheme: ColorScheme.dark(),
     iconTheme: IconThemeData(color: Colors.deepPurple.shade200,size: 40),
-    appBarTheme: AppBarTheme(
 
+    appBarTheme: AppBarTheme(
         color: Colors.black12,
         iconTheme: IconThemeData(color: Colors.deepPurple.shade200,size: 40))
   );
